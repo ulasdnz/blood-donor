57d6606b34bba96ba23ffe357ba7f04e1bfcc2e5
Ulas Deniz Isik <89457494+ulasdnz@users.noreply.github.com>
2023-02-18 16:30:08 +0300

    Update login response

--- controllers/auth.js
+++ controllers/auth.js
@@ -75,7 +75,7 @@ exports.login = (req, res, next) => {
         },
         'somesupersecretsecret'
       );
-      res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+      res.status(200).json({ token: token, user: loadedUser });
     })
     .catch(err => {
       if (!err.statusCode) {
